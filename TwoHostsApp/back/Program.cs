54398a48d28d64c130496af159993a94437acbb9
Сергей Куликов <syrex92@gmail.com>
2026-02-09 12:24:26 +0300

    git ignore

--- TwoHostsApp/back/Program.cs
+++ TwoHostsApp/back/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder =>
     {
-        builder.WithOrigins("http://localhost:5173")
+        builder.WithOrigins("https://otus-hw-front.syrex.ru.net")
                .AllowAnyHeader()
                .AllowAnyMethod();
     });
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
